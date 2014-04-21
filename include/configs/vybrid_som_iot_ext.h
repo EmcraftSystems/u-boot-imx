37757b65b9964fef77d83b83293a7bfa1a5c0133
Dmitry Konyshev <probables@emcraft.com>
2014-04-21 16:22:45 +0400

    add configuration for IOT EXT

---
+++ include/configs/vybrid_som_iot_ext.h
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2013 Emcraft Systems
+ *
+ * Configuration settings for Vybrid-SOM Rev 2A.
+ *
+ * Pavel Boldin <paboldin@emcraft.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+/*#define DEBUG*/
+
+#define CONFIG_SYS_BOARD_REV            0x2A
+
+/* #define CONFIG_SYS_ICACHE_OFF */
+/* #define CONFIG_SYS_DCACHE_OFF */
+/* #define CONFIG_SYS_ARM_CACHE_WRITETHROUGH */
+/* #define CONFIG_SYS_L2CACHE_OFF */
+
+#define CONFIG_MMC
+#define CONFIG_MCFFEC
+
+#define CONFIG_NR_DRAM_BANKS		1
+#define PHYS_SDRAM_1_SIZE		(512 * 1024 * 1024)
+
+#include "vybrid_som_common.h"
+
+#define CONFIG_VYBRID_QSPI_512MBIT_DEVICE
+
+#define CONFIG_ENV_IS_IN_QSPI_FLASH
+#define CONFIG_ENV_OFFSET		(0x40000)
+#define CONFIG_ENV_SIZE			(0x10000)
+
+#define CONFIG_LCD
+#define CONFIG_VIDEO_MVF_DCU
+#define LCD_FUSION7
+#define CONFIG_SYS_CONSOLE_IS_IN_ENV
+#define CONFIG_SYS_CONSOLE_OVERWRITE_ROUTINE
+#define CONFIG_SPLASH_SCREEN
+#define CONFIG_SPLASH_SCREEN_ALIGN
+#define CONFIG_SETUP_VIDEOLFB_TAG
+#define CONFIG_SETUP_MTDSPLASHPART_TAG
+
+#define CONFIG_MTD_SPLASH_PART_START	0x080000
+#define CONFIG_MTD_SPLASH_PART_LEN	0x180000
+
+#define CONFIG_BMP
+#undef CONFIG_CMD_BMP
+#define CONFIG_BMP_24BPP
+#define LCD_BPP	LCD_COLOR32
+
+#include "vybrid_lcd_config.h"
+
+#ifdef CONFIG_SETUP_MTDSPLASHPART_TAG
+
+# if CONFIG_MTD_SPLASH_PART_LEN != 0x180000
+#  error "Current partition config is for splash len = 0x180000"
+# endif
+
+# if CONFIG_MTD_SPLASH_PART_START != 0x080000
+#  error "Current partition config is for splash start = 0x080000"
+# endif
+
+# define KERNEL_FLASH_BASE	"200000"
+# define KERNEL_MEM_BASE	"20200000"
+
+#else /* CONFIG_SPLASH_SCREEN */
+# define KERNEL_FLASH_BASE	"80000"
+# define KERNEL_MEM_BASE	"20080000"
+#endif /* CONFIG_SPLASH_SCREEN */
+
+#define CONFIG_HOSTNAME		vf6-som
+#define CONFIG_EXTRA_ENV_SETTINGS					\
+        "autoload=yes\0"						\
+        "addip=setenv bootargs ${bootargs} "				\
+                "ip=${ipaddr}:${serverip}:${gatewayip}:"		\
+                        "${netmask}:${hostname}:eth0:off "		\
+                "fec_mac=${ethaddr}\0"					\
+        "ethaddr=C0:B1:3C:77:88:AB\0"					\
+        "ipaddr=172.17.44.46\0"						\
+        "serverip=172.17.0.1\0"						\
+        "image=uImage\0"						\
+	"netboot=tftp ${image};run addip;bootm\0"			\
+	"bootcmd=qspi probe 1;cp.b " KERNEL_MEM_BASE " ${loadaddr} "	\
+	"${flashsize};run addip;bootm\0"				\
+	"bootargs=mem=" KERNEL_MEM_INFO " console=ttymxc0,115200 "	\
+	LCD_BOOTARG "\0"						\
+	"verify=no\0"							\
+	"bootdelay=3\0"							\
+	"update=tftp ${image} && qspi probe 1 && qspi erase "		\
+	KERNEL_FLASH_BASE " +${filesize} && qspi write ${loadaddr} "	\
+	KERNEL_FLASH_BASE " ${filesize} && setenv flashsize ${filesize}"\
+	" && saveenv\0"
+#endif /* __CONFIG_H */
