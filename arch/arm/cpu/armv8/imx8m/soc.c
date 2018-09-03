c7b32da7131d7136ec40bdfb3fe27654e9aa2885
Ye Li <ye.li@nxp.com>
2018-09-03 00:46:09 -0700

    MLK-19433-2 imx8mq: Get chip rev for B1 revision
    The mscale B1 uses OCOTP_HW_OCOTP_READ_FUSE_DATA register for chip id.
    It returns a magic number 0xff0055aa.
    Update get_cpu_rev to support this way, also enable OCOTP clock to allow
    access OCOTP register.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 866631c2140b9352c6f74ec36d1a51fea40c0445)

--- arch/arm/cpu/armv8/imx8m/soc.c
+++ arch/arm/cpu/armv8/imx8m/soc.c
@@ -228,16 +228,28 @@ u32 get_cpu_rev(void)
 
 	reg &= 0xff;
 
-	if (reg == 0x10) {
-		/* For B0 chip, the DIGPROG is not updated, still TO1.0.
-		 * we have to check ROM version further
-		 */
-		uint32_t rom_version;
-		rom_version = readl((void __iomem *)0x800);
-		if (rom_version != 0x10) {
-			rom_version = readl((void __iomem *)0x83c);
-			if (rom_version >= 0x20)
-				reg = 0x20;
+	/* iMX8MM */
+	 if (major_low == 0x41) {
+		return ((type + 1) << 12) | reg;
+	} else {
+		/* iMX8MQ */
+		if (reg == 0x10) {
+			/* For B0 chip, the DIGPROG is not updated, still TO1.0.
+			 * we have to check ROM version or OCOTP_READ_FUSE_DATA
+			 */
+			if (readl((void __iomem *)(OCOTP_BASE_ADDR + 0x40))
+				== 0xff0055aa) {
+				/* 0xff0055aa is magic number for B1 */
+				reg = 0x21;
+			} else {
+				uint32_t rom_version;
+				rom_version = readl((void __iomem *)0x800);
+				if (rom_version != 0x10) {
+					rom_version = readl((void __iomem *)0x83c);
+					if (rom_version == 0x20)
+						reg = 0x20;
+				}
+			}
 		}
 	}
 
