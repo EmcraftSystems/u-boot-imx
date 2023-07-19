59b0dd524760e40126e179b18f9f4b7e2792db01
Vladimir Skvortsov <vskvortsov@emcraft.com>
2023-07-19 14:16:53 +0300

    RM 6727. IMXRT1170: Create separate config to boot from SDCARD. Move initialization of CPU frequency and external SDRAM setting from SPL to main U-Boot, to prevent duplicate clocks setting by SPL and TPL.

--- include/configs/imxrt1170-evk.h
+++ include/configs/imxrt1170-evk.h
@@ -27,9 +27,16 @@
 /* For SPL ends */
 
 #define CONFIG_EXTRA_ENV_SETTINGS					\
+	"image=rootfs.uImage\0"						\
+	"addip=setenv bootargs ${bootargs} ip=${ipaddr}:${serverip}:"	\
+		"${gatewayip}:${netmask}:${hostname}:eth0:off\0"	\
+	"mmcboot=fatload mmc 0 ${loadaddr} ${image} && run addip &&"	\
+		" bootm ${loadaddr}\0"					\
+	"netboot=tftp ${image} && run addip && bootm\0"			\
 	"ethaddr=aa:bb:cc:dd:ee:f0\0"					\
 	"serverip=172.17.0.1\0"						\
 	"ipaddr=172.17.44.111\0"					\
 	"netmask=255.255.0.0\0"						\
+	"gatewayip=172.17.0.1\0"					\
 
 #endif /* __IMXRT1170_EVK_H */
