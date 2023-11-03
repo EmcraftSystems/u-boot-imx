cdd3bbc67502e924d8cb2b1a20c04df96c2059c0
Vladimir Skvortsov <vskvortsov@emcraft.com>
2023-11-03 09:54:31 +0300

    RM-6664: configs: imxrt1170-evk-qspi: Set default boot command to boot from the SPI Flash in the QSPI configuration of the IMXRT1170-EVK board.

--- include/configs/imxrt1170-evk.h
+++ include/configs/imxrt1170-evk.h
@@ -30,6 +30,37 @@
 #define CFG_SYS_UBOOT_BASE	        0x30010000
 /* For SPL ends */
 
+#if defined(CONFIG_CMD_SF)
+#define _CFG_EXTRA_SF_ENV_SETTINGS					\
+	"sfboot=sf probe 0 && sf read ${loadaddr}"			\
+		" ${kernel_sf_offset} ${kernel_sf_size} &&"		\
+		" run addip && run addrootfs && bootm ${loadaddr}\0"	\
+	"addrootfs=if test ${fstype} = \"ubi\"; then"			\
+			" setenv bootargs ${bootargs} ${argsubi};"	\
+		" elif test ${fstype} = \"jffs2\"; then"		\
+			" setenv bootargs ${bootargs} ${argsjffs2};"	\
+		" fi;\0"						\
+	"project=rootfs_flash\0"					\
+	"fstype=jffs2\0"						\
+	"argsubi=ubi.mtd=3 rootfstype=ubifs root=ubi0:rootfs rw\0"	\
+	"argsjffs2=rootfstype=jffs2 root=/dev/mtd3 rw\0"		\
+	"uboot_sf_offset=0x0\0"						\
+	"uboot_sf_size=0x60000\0"					\
+	"kernel_sf_offset=0x80000\0"					\
+	"kernel_sf_size=0x800000\0"					\
+	"rootfs_sf_offset=0x880000\0"					\
+	"rootfs_sf_size=0x780000\0"					\
+	"sf_kernel_update=tftp ${tftpdir}${project}.uImage &&"		\
+		" sf erase ${kernel_sf_offset} ${kernel_sf_size} &&"	\
+		" sf write ${loadaddr} ${kernel_sf_offset} ${filesize}\0"\
+	"sf_rootfs_update=tftp ${tftpdir}${project}.${fstype} &&"	\
+		" sf erase ${rootfs_sf_offset} ${rootfs_sf_size} &&"	\
+		" sf write ${loadaddr} ${rootfs_sf_offset} ${filesize}\0"\
+
+#else
+#define _CFG_EXTRA_SF_ENV_SETTINGS
+#endif
+
 #define CFG_EXTRA_ENV_SETTINGS						\
 	"image=rootfs.uImage\0"						\
 	"uboot=u-boot.img\0"						\
@@ -55,5 +86,6 @@
 	"ipaddr=172.17.44.111\0"					\
 	"netmask=255.255.0.0\0"						\
 	"gatewayip=172.17.0.1\0"					\
+	_CFG_EXTRA_SF_ENV_SETTINGS
 
 #endif /* __IMXRT1170_EVK_H */
