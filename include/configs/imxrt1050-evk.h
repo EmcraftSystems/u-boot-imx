6e0b41c978b3dc305e66b8dd8dc630bccb002e0b
Alexander Sanochkin <sasha@emcraft.com>
2024-09-21 12:45:28 +0300

    RM-7148: IMXRT1060: Basic port u-boot to imxrt1060-evk
    (cherry picked from commit 4f1253eeb27665a1844fd319a3cdfad76327077a)
    (cherry picked from commit 92b7ad11ae912de1a64c026cd94b7d39c1583f8d)

--- include/configs/imxrt1050-evk.h
+++ include/configs/imxrt1050-evk.h
@@ -19,7 +19,6 @@
 #define DMAMEM_SZ_ALL			(1 * 1024 * 1024)
 #define DMAMEM_BASE			(PHYS_SDRAM + PHYS_SDRAM_SIZE - \
 					 DMAMEM_SZ_ALL)
-#define CONFIG_SYS_BOOTM_LEN		SZ_32M
 
 #ifdef CONFIG_DM_VIDEO
 #define CONFIG_VIDEO_BMP_LOGO
@@ -32,24 +31,45 @@
 #define _CONFIG_EXTRA_ENV_SETTINGS_VIDEO ""
 #endif
 
+#if CONFIG_IS_ENABLED(TARGET_IMXRT1050_EVK)
+#define _CONFIG_TFTPDIR	"tftpdir=imxrt1050/\0"
+#elif CONFIG_IS_ENABLED(TARGET_IMXRT1060_EVK)
+#define _CONFIG_TFTPDIR	"tftpdir=imxrt1060/\0"
+#else
+#define _CONFIG_TFTPDIR	""
+#endif
+
 /*
  * Address of U-Boot for SPI NOR boot
  */
 
 #define CFG_SYS_UBOOT_BASE			0x60010000
 
-#define CONFIG_EXTRA_ENV_SETTINGS \
-	"image=rootfs.uImage\0" \
+#define CFG_EXTRA_ENV_SETTINGS						\
+	"image=rootfs.uImage\0"						\
+	"uboot=u-boot.img\0"						\
+	"spl=SPL\0"							\
+	"mmc_update_spl=tftp ${tftpdir}${spl} &&"			\
+                " setexpr tmp ${filesize} / 0x200 &&"			\
+		" setexpr tmp ${tmp} + 1 &&"				\
+                " mmc write ${loadaddr} 2 ${tmp}\0"			\
+	"mmc_update_uboot=tftp ${tftpdir}${uboot} &&"			\
+                " setexpr tmp ${filesize} / 0x200 &&"			\
+		" setexpr tmp ${tmp} + 1 &&"				\
+                " mmc write ${loadaddr} 0x100 ${tmp}\0"			\
+        "mmc_update_kernel=tftp ${tftpdir}${image} &&"			\
+                " fatwrite mmc 0 ${loadaddr} ${image} ${filesize}\0"	\
 	"addip=setenv bootargs ${bootargs} ip=${ipaddr}:${serverip}:"	\
 		"${gatewayip}:${netmask}:${hostname}:eth0:off\0"	\
 	"mmcboot=fatload mmc 0 ${loadaddr} ${image} && run addip &&"	\
-		" bootm ${loadaddr}\0" \
-	"netboot=tftp ${image} && run addip && bootm\0"			\
+		" bootm ${loadaddr}\0"					\
+	"netboot=tftp ${tftpdir}${image} && run addip && bootm\0"	\
 	"ethaddr=aa:bb:cc:dd:ee:d0\0"					\
 	"serverip=172.17.0.1\0"						\
 	"gatewayip=172.17.0.1\0"					\
 	"ipaddr=172.17.44.105\0"					\
 	"netmask=255.255.0.0\0"						\
+	_CONFIG_TFTPDIR							\
 	_CONFIG_EXTRA_ENV_SETTINGS_VIDEO
 
 #endif /* __IMXRT1050_EVK_H */
