2f648b89229fbcae7c43222c83fb61630dbac742
Alexander Sanochkin <sasha@emcraft.com>
2025-01-13 16:14:45 +0300

    RM 6727. IMXRT1170: Create separate config to boot from SDCARD. Move initialization of CPU frequency and external SDRAM setting from SPL to main U-Boot, to prevent duplicate clocks setting by SPL and TPL.
    (cherry picked from commit 59b0dd524760e40126e179b18f9f4b7e2792db01)
    (cherry picked from commit 583802dd9509eda124186d134d3b1c2b8be4a0fc)

--- board/freescale/imxrt1170-evk/imxrt1170-evk.c
+++ board/freescale/imxrt1170-evk/imxrt1170-evk.c
@@ -17,6 +17,8 @@
 #include <serial.h>
 #include <phy.h>
 #include <linux/delay.h>
+#include <clk.h>
+#include <dt-bindings/clock/imxrt1170-clock.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -224,6 +226,14 @@ int board_early_init_f(void)
 
 int board_early_init_r(void)
 {
+	/* Switch M7 CPU core to 986MHz from ARM_PLL */
+	struct clk *clk, *clk1;
+
+	clk_get_by_id(IMXRT1170_CLK_PLL_ARM_OUT, &clk);
+	clk_enable(clk);
+	clk_get_by_id(IMXRT1170_CLK_ROOT_M7, &clk1);
+	clk_set_parent(clk1, clk);
+
 	int rv;
 	struct udevice *dev;
 
@@ -233,7 +243,6 @@ int board_early_init_r(void)
 	}
 	return rv;
 }
-
 
 #endif
 
@@ -268,21 +277,9 @@ int spl_start_uboot(void)
 }
 #endif
 
-int spl_dram_init(void)
-{
-	struct udevice *dev;
-	int rv;
-
-	rv = uclass_get_device(UCLASS_RAM, 0, &dev);
-	if (rv)
-		debug("DRAM init failed: %d\n", rv);
-	return rv;
-}
-
 void spl_board_init(void)
 {
 	preloader_console_init();
-	spl_dram_init();
 	arch_cpu_init(); /* to configure mpu for sdram rw permissions */
 }
 
