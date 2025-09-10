EmcraftSystems/u-boot-fsl

RM-1995 bootm: support for 2-files multi-image format
This is to be able to boot kernel Image + DTB as one multi-image file.

Signed-off-by: Yuri Tikhonov <yur@emcraft.com>

common/image-fdt.c
@@ -165,6 +165,16 @@ int boot_relocate_fdt(struct lmb *lmb, char **of_flat_tree, ulong *of_size)
 		goto error;
 	}
 
+#if defined(CONFIG_OF_FORCE_RELOCATE)
+	/*
+	 * If force dtb relocation is requested, then move dtb to the bootmap
+	 * end. You may want to avoid such force relocation if booting dtb
+	 * separately (i.e. not as a part of multi-image)
+	 */
+	if (!getenv ("fdt_no_force_reloc"))
+		disable_relocation = 0;
+#endif
+
 	if (disable_relocation) {
 		/*
 		 * We assume there is space after the existing fdt to use
@@ -389,16 +399,17 @@ int boot_get_fdt(int flag, int argc, char * const argv[], uint8_t arch,
 	} else if (images->legacy_hdr_valid &&
 			image_check_type(&images->legacy_hdr_os_copy,
 					 IH_TYPE_MULTI)) {
-		ulong fdt_data, fdt_len;
+		ulong fdt_data, fdt_len, idx;
 
 		/*
 		 * Now check if we have a legacy multi-component image,
-		 * get second entry data start address and len.
+		 * get the appropriate entry data start address and len.
 		 */
 		printf("## Flattened Device Tree from multi component Image at %08lX\n",
 		       (ulong)images->legacy_hdr_os);
 
-		image_multi_getimg(images->legacy_hdr_os, 2, &fdt_data,
+		idx = (image_multi_count (images->legacy_hdr_os) == 2) ? 1 : 2;
+		image_multi_getimg(images->legacy_hdr_os, idx, &fdt_data,
 				   &fdt_len);
 		if (fdt_len) {
 			fdt_blob = (char *)fdt_data;

EmcraftSystems/u-boot-imx

(cherry picked from commit a96ec92609d56fe918691a4aeb1e29e239e3cea4)
(cherry picked from commit 7ee500fab5532f1734f9e2fbb9bd993ae6d5fa94)

boot/image-fdt.c
@@ -238,6 +238,16 @@ int boot_relocate_fdt(char **of_flat_tree, ulong *of_size)
 		goto error;
 	}
 
+#if defined(CONFIG_OF_FORCE_RELOCATE)
+	/*
+	 * If force dtb relocation is requested, then move dtb to the bootmap
+	 * end. You may want to avoid such force relocation if booting dtb
+	 * separately (i.e. not as a part of multi-image)
+	 */
+	if (!env_get ("fdt_no_force_reloc"))
+		disable_relocation = 0;
+#endif
+
 	if (disable_relocation) {
 		/*
 		 * We assume there is space after the existing fdt to use
@@ -462,16 +472,17 @@ int boot_get_fdt(void *buf, const char *select, uint arch,
 	} else if (images->legacy_hdr_valid &&
 			image_check_type(&images->legacy_hdr_os_copy,
 					 IH_TYPE_MULTI)) {
-		ulong fdt_data, fdt_len;
+		ulong fdt_data, fdt_len, idx;
 
 		/*
 		 * Now check if we have a legacy multi-component image,
-		 * get second entry data start address and len.
+		 * get the appropriate entry data start address and len.
 		 */
 		printf("## Flattened Device Tree from multi component Image at %08lX\n",
 		       (ulong)images->legacy_hdr_os);
 
-		image_multi_getimg(images->legacy_hdr_os, 2, &fdt_data,
+		idx = (image_multi_count (images->legacy_hdr_os) == 2) ? 1 : 2;
+		image_multi_getimg(images->legacy_hdr_os, idx, &fdt_data,
 				   &fdt_len);
 		if (fdt_len) {
 			fdt_blob = (char *)fdt_data;