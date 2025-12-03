643341dddd93c5570d9c414cc7a8afb49d792cf0
Alexander Sanochkin <sasha@emcraft.com>
2025-12-03 10:38:25 +0300

    RM-7535: imxrt1024: Port i.MX 1024 U-Boot to 6.12.20 BSP

--- include/configs/imxrt1024-evk.h
+++ include/configs/imxrt1024-evk.h
@@ -9,8 +9,6 @@
 
 #include <asm/arch/imx-regs.h>
 
-#define CONFIG_SYS_INIT_SP_ADDR		0x20240000
-
 #define ESDHCI_QUIRK_BROKEN_TIMEOUT_VALUE	1
 
 #define PHYS_SDRAM			0x80000000
@@ -19,21 +17,7 @@
 #define DMAMEM_SZ_ALL			(1 * 1024 * 1024)
 #define DMAMEM_BASE			(PHYS_SDRAM + PHYS_SDRAM_SIZE - \
 					 DMAMEM_SZ_ALL)
-#define CONFIG_SYS_BOOTM_LEN		SZ_32M
-
-/*
- * Configuration of the external SDRAM memory
- */
-
-/* For SPL */
-#ifdef CONFIG_SUPPORT_SPL
-#define CONFIG_SPL_STACK		CONFIG_SYS_INIT_SP_ADDR
-#define CONFIG_SYS_SPL_LEN		0x00008000
-#define CONFIG_SYS_UBOOT_START		0x800023FD
-#endif
-/* For SPL ends */
-
-#define CONFIG_EXTRA_ENV_SETTINGS \
+#define CFG_EXTRA_ENV_SETTINGS						\
 	"image=rootfs.uImage\0"						\
 	"uboot=u-boot.img\0"						\
 	"spl=SPL\0"							\
@@ -52,7 +36,7 @@
 		"${gatewayip}:${netmask}:${hostname}:eth0:off\0"	\
 	"mmcboot=fatload mmc 0 ${loadaddr} ${image} && run addip &&"	\
 		" bootm ${loadaddr}\0"					\
-	"netboot=tftp ${tftpdir}${image} && run addip && bootm\0"			\
+	"netboot=tftp ${tftpdir}${image} && run addip && bootm\0"	\
 	"ethaddr=aa:bb:cc:dd:ee:e0\0"					\
 	"serverip=172.17.0.1\0"						\
 	"gatewayip=172.17.0.1\0"					\
